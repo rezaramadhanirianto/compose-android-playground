7d89a0373f7af4be2c9dbf241e5456472b941127
rezaramadhanirianto <rezaramadhanirianto@gmail.com>
2024-05-06 22:32:46 +0700

    add flickering UI

--- app/src/main/java/com/example/composeandroidplayground/combineWithXml/CustomCompose.kt
+++ app/src/main/java/com/example/composeandroidplayground/combineWithXml/CustomCompose.kt
@@ -6,20 +6,20 @@ import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.MutableState
+import androidx.compose.runtime.mutableIntStateOf
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.unit.dp
+import kotlin.random.Random
 
 @Composable
-fun RectangleMatchParentWithHeight(booleanState: MutableState<Boolean>) {
-    if (booleanState.value) {
+fun RectangleMatchParentWithHeight(heightState: MutableState<Int>) {
         Box(
             modifier = Modifier
                 .fillMaxWidth()
-                .height(200.dp)
+                .height(heightState.value.dp)
                 .background(Color.Cyan)
         )
-    } else {
-        Box(modifier = Modifier)
-    }
 }
--- app/src/main/java/com/example/composeandroidplayground/combineWithXml/MyViewHolder.kt
+++ app/src/main/java/com/example/composeandroidplayground/combineWithXml/MyViewHolder.kt
@@ -1,9 +1,14 @@
 package com.example.composeandroidplayground.combineWithXml
 
+import android.content.Context
+import android.util.TypedValue
 import android.view.LayoutInflater
+import android.view.View
 import android.view.ViewGroup
+import android.view.ViewTreeObserver
 import android.widget.FrameLayout
 import androidx.compose.runtime.MutableState
+import androidx.compose.runtime.mutableIntStateOf
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.ui.platform.ComposeView
@@ -14,28 +19,22 @@ import kotlin.random.Random
 
 class MyViewHolder(
     private val binding: TextRectangleViewLayoutBinding,
-    private val state: MutableState<Boolean>
+    private val state: MutableState<Int>
 ) : RecyclerView.ViewHolder(binding.root) {
+
     fun bind(text: String) {
-        val randomBoolean = randomBoolean()
         binding.textView.text = text
-        binding.composeView.isVisible = randomBoolean
 
-        // Update the state value if it has changed
-        if (state.value != randomBoolean) {
-            state.value = randomBoolean
-        }
-    }
+        val height = Random.nextInt(100, 501)
 
-    private fun randomBoolean(): Boolean {
-        return Random.nextBoolean()
+        state.value = height
     }
 
     companion object {
         fun create(parent: ViewGroup): MyViewHolder {
             val inflater = LayoutInflater.from(parent.context)
             val binding = TextRectangleViewLayoutBinding.inflate(inflater, parent, false)
-            val state = mutableStateOf(Random.nextBoolean())
+            val state = mutableIntStateOf(Random.nextInt(100, 501)) // Random height between 100 and 500
             binding.frameLayout.addView(
                 ComposeView(binding.root.context).apply {
                     layoutParams = FrameLayout.LayoutParams(
